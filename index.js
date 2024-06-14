36a6c399ee5dfdb66bd6b2c971dbef6e05c68194
Gsrcordeiro <cordeirogb@hotmail.com>
2024-06-14 17:53:58 -0300

    OK

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const express = require('express');
 const db = require('./db');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
+// Endpoint para verificar se a aplicação está viva
+app.get('/liveness', (req, res) => {
+    res.status(200).send('Aplicação está viva');
+});
+
+// Endpoint para verificar se a aplicação está pronta para receber tráfego
+app.get('/readiness', (req, res) => {
+    // Você pode adicionar verificações adicionais aqui, se necessário
+    res.status(200).send('Aplicação está pronta para receber tráfego');
+});
+
+// Endpoint para consultar dados do banco de dados
 app.get('/consulta-dados', (req, res) => {
     db.query('SELECT * FROM exemplo', (err, results) => {
         if (err) {
@@ -14,6 +26,7 @@ app.get('/consulta-dados', (req, res) => {
     });
 });
 
+// Inicia o servidor na porta especificada
 app.listen(port, () => {
     console.log(`Aplicação rodando em http://localhost:${port}`);
 });
