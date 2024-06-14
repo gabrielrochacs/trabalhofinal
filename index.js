0c2f8d39e79d019686756125e0499c9afd0bd711
Gsrcordeiro <cordeirogb@hotmail.com>
2024-06-14 18:16:59 -0300

    OK

--- index.js
+++ index.js
@@ -4,21 +4,10 @@ const db = require('./db');
 const app = express();
 const port = 3000;
 
-// Endpoint para verificar se a aplicação está viva
-app.get('/liveness', (req, res) => {
-    res.status(200).send('Aplicação está viva');
-});
-
-// Endpoint para verificar se a aplicação está pronta para receber tráfego
-app.get('/readiness', (req, res) => {
-    // Você pode adicionar verificações adicionais aqui, se necessário
-    res.status(200).send('Aplicação está pronta para receber tráfego');
-});
-
-// Endpoint para consultar dados do banco de dados
 app.get('/consulta-dados', (req, res) => {
-    db.query('SELECT * FROM exemplo', (err, results) => {
+    db.query('SELECT * FROM usuarios', (err, results) => {
         if (err) {
+            console.error('Erro ao consultar os dados:', err);
             res.status(500).send('Erro ao consultar os dados');
             return;
         }
@@ -26,7 +15,20 @@ app.get('/consulta-dados', (req, res) => {
     });
 });
 
-// Inicia o servidor na porta especificada
+app.get('/liveness', (req, res) => {
+    res.status(200).send('OK');
+});
+
+app.get('/readiness', (req, res) => {
+    db.query('SELECT 1', (err, results) => {
+        if (err) {
+            res.status(500).send('Erro ao consultar o banco de dados');
+            return;
+        }
+        res.status(200).send('OK');
+    });
+});
+
 app.listen(port, () => {
     console.log(`Aplicação rodando em http://localhost:${port}`);
 });
