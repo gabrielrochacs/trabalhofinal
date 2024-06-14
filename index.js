fa948a6486819d20df2ebd224e28fa2570f69342
Gsrcordeiro <cordeirogb@hotmail.com>
2024-06-14 17:56:33 -0300

    OK

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const db = require('./db');
 
 const app = express();
-const port = 3001;
+const port = 3000;
 
 // Endpoint para verificar se a aplicação está viva
 app.get('/liveness', (req, res) => {
