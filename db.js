fe0c6265ae2d785c773b435cb9ed57bd0dc83cea
Gsrcordeiro <cordeirogb@hotmail.com>
2024-06-14 20:53:15 -0300

    OK

--- db.js
+++ db.js
@@ -1,18 +1,16 @@
 const mysql = require('mysql');
-require('dotenv').config(); // Se estiver usando variáveis de ambiente
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST || 'mysql',
-    user: process.env.DB_USER || 'user',
-    password: process.env.DB_PASSWORD || 'passwd',
-    database: process.env.DB_NAME || 'db_aula',
-    multipleStatements: true // Permite múltiplas consultas em uma única string
+    host: 'mysql',
+    user: 'user',
+    password: 'passwd',
+    database: 'db_aula'
 });
 
 connection.connect((err) => {
     if (err) {
         console.error('Erro ao conectar no banco de dados:', err);
-        throw err; // Se a conexão falhar, interrompe a execução
+        return;
     }
     console.log('Conectado ao banco de dados MySQL');
 });
