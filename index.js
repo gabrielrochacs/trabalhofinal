e5896c37237ec527e03a4babd36317e87b9a0c1b
Gsrcordeiro <cordeirogb@hotmail.com>
2024-06-15 00:49:04 -0300

    Final1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const db = require('./db');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.get('/consulta-dados', (req, res) => {
     db.query('SELECT * FROM exemplo', (err, results) => {
